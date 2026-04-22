aaf41640965048392765e78fea72bf3ef4f96c09
ErimRank <erimrank167@gmail.com>
2026-04-22 08:37:06 +0300

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { SERVERCONNECTION } from "./Library/Connection/Connection.js";
+import { ENVIRONMENTCONFIG } from "./Library/Environment/Connection.js";
 import { FUNCTIONS } from "./Library/Functions/Connection.js";
 
 export const CONNECTION=()=>{
+    ENVIRONMENTCONFIG();
     SERVERCONNECTION();
     FUNCTIONS();
 };
