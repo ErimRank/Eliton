154a5788f3efa0698820d3294e5ee860c94416dc
ErimRank <erimrank167@gmail.com>
2026-04-24 08:44:53 +0300

    Updates

--- Library/Functions/Connection.js
+++ Library/Functions/Connection.js
@@ -1,12 +1,10 @@
 import { ADD } from "./DataBase/Add/Add.js";
-import { AUTORUN } from "./DataBase/Auto/Auto.js";
 import { CLEAR } from "./DataBase/Clear/Clear.js";
 import { CONDITION } from "./DataBase/Condition/Condition.js";
 import { DISPLAY } from "./DataBase/Display/Display.js";
 
 export const FUNCTIONS=()=>{
 const DATA=`const ADD=${ADD};
-const AUTORUN=${AUTORUN};
 const CLEAR=${CLEAR};
 const CONDITION=${CONDITION};
 const DISPLAY=${DISPLAY};
@@ -14,5 +12,5 @@ const DISPLAY=${DISPLAY};
 localStorage.setItem("FUNCTIONS",DATA);
 };
 `
-const AUTORUN=${AUTORUN};
+const START=${DISPLAY};
 `
